fd5bcf6142bcba33e84d39f845fbd698e71c8590
13reathcode <new13reath@gmail.com>
2025-04-09 01:20:25 +0300

    final ver

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const content = {
   Monthly_previous: ["128hrs", "29hrs", "19hrs", "18hrs", "23hrs", "11hrs"],
 };
 
+const titles = ["Work", "Play", "Study", "Exercise", "Social", "Self care"];
+
 function App() {
   const [activeTimeline, setActiveTimeline] = useState("Daily_active");
   const [previousTimeline, setPreviousTimeline] = useState("Daily_previous");
@@ -34,146 +36,71 @@ function App() {
 
   return (
     <main className="cards-deck">
-      <div className="card-box-profile">
-        <div className="card-profile a">
-          <img
-            className="card-profile-avatar"
-            src={process.env.PUBLIC_URL + "/images/image-jeremy.png"}
-            alt="avatar"
-          />
-
-          <div className="card-profile-info">
-            <span>Report for</span>
-            <p>Jeremy Robson</p>
-          </div>
-        </div>
-
-        <div className="card-profile-buttons">
-          <button onClick={() => handleClick("Daily")}>Daily</button>
-          <button onClick={() => handleClick("Weekly")}>Weekly</button>
-          <button onClick={() => handleClick("Monthly")}>Monthly</button>
-        </div>
-      </div>
-
-      <div className="card-box b ">
-        <img
-          className="card-box-image"
-          src={process.env.PUBLIC_URL + "/images/icon-work.svg"}
-          alt=""
+      <CardProfile onClick={handleClick} />
+      {titles.map((title, index) => (
+        <Card
+          key={title}
+          title={title}
+          index={index}
+          current={activeTimeline}
+          previous={previousTimeline}
+          period={period}
         />
-        <div className="card-inner">
-          <div className="card-theme">
-            <p>Work</p>
-            <img
-              className="card-theme-image"
-              src={process.env.PUBLIC_URL + "/images/icon-ellipsis.svg"}
-              alt=""
-            />
-          </div>
-          <div className="card-time">
-            <p>{content[activeTimeline][0]}</p>
-            <span>
-              {period} - {content[previousTimeline][0]}
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="card-box c ">
-        <div className="card-inner">
-          <div className="card-theme">
-            <p>Play</p>
-            <img
-              className="card-theme-image"
-              src={process.env.PUBLIC_URL + "/images/icon-ellipsis.svg"}
-              alt=""
-            />
-          </div>
-          <div className="card-time">
-            <p>{content[activeTimeline][1]}</p>
-            <span>
-              {period} - {content[previousTimeline][1]}
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="card-box d">
-        <div className="card-inner">
-          <div className="card-theme">
-            <p>Study</p>
-            <img
-              className="card-theme-image"
-              src={process.env.PUBLIC_URL + "/images/icon-ellipsis.svg"}
-              alt=""
-            />
-          </div>
-          <div className="card-time">
-            <p>{content[activeTimeline][2]}</p>
-            <span>
-              {period} - {content[previousTimeline][2]}
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="card-box e">
-        <div className="card-inner">
-          <div className="card-theme">
-            <p>Exercise</p>
-            <img
-              className="card-theme-image"
-              src={process.env.PUBLIC_URL + "/images/icon-ellipsis.svg"}
-              alt=""
-            />
-          </div>
-          <div className="card-time">
-            <p>{content[activeTimeline][3]}</p>
-            <span>
-              {period} - {content[previousTimeline][3]}
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="card-box f">
-        <div className="card-inner">
-          <div className="card-theme">
-            <p>Social</p>
-            <img
-              className="card-theme-image"
-              src={process.env.PUBLIC_URL + "/images/icon-ellipsis.svg"}
-              alt=""
-            />
-          </div>
-          <div className="card-time">
-            <p>{content[activeTimeline][4]}</p>
-            <span>
-              {period} - {content[previousTimeline][4]}
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="card-box g">
-        <div className="card-inner">
-          <div className="card-theme">
-            <p>Self care</p>
-            <img
-              className="card-theme-image"
-              src={process.env.PUBLIC_URL + "/images/icon-ellipsis.svg"}
-              alt=""
-            />
-          </div>
-          <div className="card-time">
-            <p>{content[activeTimeline][5]}</p>
-            <span>
-              {period} - {content[previousTimeline][5]}
-            </span>
-          </div>
-        </div>
-      </div>
+      ))}
     </main>
+  );
+}
+
+function Card({ title, index, current, previous, period }) {
+  return (
+    <div className={`card-box card-${index}`}>
+      <img
+        className="card-box-image"
+        src={process.env.PUBLIC_URL + `/images/icon-${title.toLowerCase()}.svg`}
+        alt=""
+      />
+      <div className="card-inner">
+        <div className="card-theme">
+          <p>{title}</p>
+          <img
+            className="card-theme-image"
+            src={process.env.PUBLIC_URL + "/images/icon-ellipsis.svg"}
+            alt=""
+          />
+        </div>
+        <div className="card-time">
+          <p>{content[current][index]}</p>
+          <span>
+            {period} - {content[previous][index]}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CardProfile({ onClick }) {
+  return (
+    <div className="card-box-profile card-x">
+      <div className="card-profile">
+        <img
+          className="card-profile-avatar"
+          src={process.env.PUBLIC_URL + "/images/image-jeremy.png"}
+          alt="avatar"
+        />
+
+        <div className="card-profile-info">
+          <span>Report for</span>
+          <p>Jeremy Robson</p>
+        </div>
+      </div>
+
+      <div className="card-profile-buttons">
+        <button onClick={() => onClick("Daily")}>Daily</button>
+        <button onClick={() => onClick("Weekly")}>Weekly</button>
+        <button onClick={() => onClick("Monthly")}>Monthly</button>
+      </div>
+    </div>
   );
 }
 
